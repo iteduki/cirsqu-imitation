b82b65b9f752efb90b1d3e832a0ccbb405f6fd47
Takahiro Ogawa <mistolteen0127@gmail.com>
2019-03-14 17:47:38 +0900

    seed作成

---
+++ db/seeds/development/user.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+FactoryBot.create_list(:user, 100)
+
+FactoryBot.create(:user, :admin)
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,17 @@
 
 FactoryBot.define do
   factory :user do
-    name { 'MyString' }
-    email { 'MyString' }
+    sequence(:email) { |n| "test#{format('%05d', n)}@example.com" }
+    sequence(:family_name) { |n| n.even? ? '山田' : '田中' }
+    sequence(:given_name) { |n| n.even? ? "太郎#{n}" : "花子#{n}" }
+    sequence(:nickname) { |n| n.even? ? "ヤマダタロウ#{n}" : "タナカハナコ#{n}" }
+    sequence(:gender_id) { |n| n.even? ? Gender::MAN.id : Gender::WOMAN.id }
+    birthday { Date.today }
+    role_id { Role::GENERAL.id }
+    sequence(:twitter) { |n| "twitter#{n}" }
+
+    trait :admin do
+      role_id { Role::ADMINISTRATOR.id }
+    end
   end
 end
